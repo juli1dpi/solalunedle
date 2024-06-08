5c23e3eb08873513f5780f436ca65d4da45c41e7
juli1dpi <patibul43@gmail.com>
2024-06-08 20:11:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var message = "Bienvenue sur Mon Site Web !";
-    var messageElement = document.createElement('p');
-    messageElement.textContent = message;
-    document.body.appendChild(messageElement);
-});
+// URL du fichier JSON
+var url = 'chanson.json';
 
+// Requête HTTP pour récupérer le fichier JSON
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    // Analyser les données JSON
+    // Par exemple, accéder à la chanson1 et à sa couverture
+    var chanson1 = data.chansons.chanson1;
+    var coverChanson1 = data.chansons.cover;
+
+    // Utiliser les données dans votre site web
+    console.log("Chanson 1:", chanson1);
+    console.log("URL de la couverture de la chanson 1:", coverChanson1);
+
+    // Vous pouvez maintenant utiliser ces données pour afficher du contenu dans votre site web
+    // Par exemple, injecter ces données dans le DOM pour les afficher sur la page
+  })
+  .catch(error => {
+    console.error('Erreur lors du chargement du fichier JSON:', error);
+  });
