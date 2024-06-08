80f857c750cc3de7bdad683f7ec631afb7f864e6
juli1dpi <patibul43@gmail.com>
2024-06-08 20:47:39 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,12 @@
 fetch('chanson.json')
   .then(response => response.json())
   .then(data => {
-    // Récupérer l'URL de l'image des paroles de la chanson1
+    // Récupérer l'URL de l'image des paroles de la chanson 1
     const lyricsImageUrl = data.chansons.chanson1.parole;
 
-    // Sélectionner la balise img
+    // Sélectionner la balise img des paroles
     const lyricsImage = document.getElementById('lyricsImage');
 
-    // Assigner l'URL de l'image des paroles à la balise img
+    // Assigner l'URL à la balise img des paroles
     lyricsImage.src = lyricsImageUrl;
   });
